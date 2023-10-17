af8a5c02f10107279627ce26732900536be74f45
Teja vamshi <123547867+tejavamshikappala@users.noreply.github.com>
2023-10-17 10:27:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+import React from "react"; 
+import { Component } from "react";
+
+import './App.css';
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+} */
+
+class App extends Component{
+  
+
+  render(){
+    return(<div className="backgroundColor">
+    <h1>hello nani</h1></div>)
+  }
+}
+
+
+
+export default App;
